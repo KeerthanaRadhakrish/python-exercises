7400029e11dc33407361bea87cd00d3b8ef514da
KeerthanaRadhakrish <keerthanaradhakrishnan13@gmail.com>
2019-07-30 00:43:36 -0700

    first commit

---
+++ CharacterInput.py
@@ -0,0 +1,12 @@
+#character input
+name=str(input("enter your name: "))
+print(name)
+age=int(input("enter your age: "))
+print(age)
+years=100-age
+print("In",years,"years, you will be 100 years old",name)
+
+
+
+
+
---
+++ odd or even.py
@@ -0,0 +1,10 @@
+yournumber = int(input("enter a number: "))
+i = yournumber
+if i % 2 == 0:
+    print("its an even number")
+else:
+    print("its an odd number")
+
+
+
+
